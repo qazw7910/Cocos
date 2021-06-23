cc92dfaaec1d8ba5496916d27b130b67236bff3c
qazw7910 <okmijn7980@gmail.com>
2021-06-23 20:57:15 +0800

    add web page

---
+++ src/settings.a60fe.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/texture/Player Start.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"c5790"}};
